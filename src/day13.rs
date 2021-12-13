8485dffc9a0eaf194c175d4f2e3cc9bbd8ae3aa4
Christian Ocker <christian.ocker@here.com>
2021-12-13 11:31:33 +0100

    Clean up day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,45 +1,31 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
-pub fn solve(input: &str) -> usize {
-    let mut stars = HashSet::new();
-    let mut fold = false;
-    let mut stars_after_first_fold = None;
-
-    for line in input.lines() {
-        if !fold {
-            if line.is_empty() {
-                fold = true;
-            } else if let [x, y] = line
-                .split(',')
+fn parse_stars(input: &str) -> HashSet<(u32, u32)> {
+    input
+        .lines()
+        .map(|line| {
+            line.split(',')
                 .map(|x| x.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-                .as_slice()
-            {
-                stars.insert((*x, *y));
-            }
-        } else if let (Some(axis), Some(coord)) = scan_fmt!(line, "fold along {}={d}", String, u32)
-        {
-            match axis.as_ref() {
-                "x" => {
-                    stars = stars
-                        .into_iter()
-                        .map(|(x, y)| ((if x > coord { 2 * coord - x } else { x }, y)))
-                        .collect();
-                }
-                "y" => {
-                    stars = stars
-                        .into_iter()
-                        .map(|(x, y)| (x, (if y > coord { 2 * coord - y } else { y })))
-                        .collect();
-                }
-                _ => {}
-            }
-            if stars_after_first_fold.is_none() {
-                stars_after_first_fold = Some(stars.len())
-            }
-        }
-    }
+                .next_tuple()
+                .unwrap()
+        })
+        .collect()
+}
 
+fn fold(input: HashSet<(u32, u32)>, fold_x: u32, fold_y: u32) -> HashSet<(u32, u32)> {
+    input
+        .into_iter()
+        .map(|(x, y)| {
+            (
+                if x > fold_x { 2 * fold_x - x } else { x },
+                if y > fold_y { 2 * fold_y - y } else { y },
+            )
+        })
+        .collect()
+}
+
+fn print_stars(stars: &HashSet<(u32, u32)>) {
     let width = stars.iter().map(|(x, _)| *x).max().unwrap();
     let height = stars.iter().map(|(_, y)| *y).max().unwrap();
     for y in 0..=height {
@@ -48,5 +34,26 @@ pub fn solve(input: &str) -> usize {
         }
         println!("");
     }
+}
+
+pub fn solve(input: &str) -> usize {
+    let mut input = input.split("\n\n");
+    let mut stars = parse_stars(input.next().unwrap());
+    let mut stars_after_first_fold = None;
+
+    for line in input.next().unwrap().lines() {
+        if let (Some(axis), Some(coord)) = scan_fmt!(line, "fold along {}={d}", String, u32) {
+            stars = if axis == "x" {
+                fold(stars, coord, u32::MAX)
+            } else {
+                fold(stars, u32::MAX, coord)
+            };
+            if stars_after_first_fold.is_none() {
+                stars_after_first_fold = Some(stars.len())
+            }
+        }
+    }
+    print_stars(&stars);
+
     stars_after_first_fold.unwrap()
 }
